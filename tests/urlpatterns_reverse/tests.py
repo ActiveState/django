10b97225ddaacd83c9770d4985e4d0927747753b
Frederick Price <rprice@pricemail.ca>
2022-02-16 19:32:20 -0500

    CVE-2021-44420 Add in tests to ensure that an error is raised when URLs with newlines are used

--- tests/urlpatterns_reverse/tests.py
+++ tests/urlpatterns_reverse/tests.py
@@ -1157,3 +1157,16 @@ class LookaheadTests(SimpleTestCase):
             reverse('lookbehind-positive', kwargs={'city': 'other-city'})
         with self.assertRaises(NoReverseMatch):
             reverse('lookbehind-negative', kwargs={'city': 'not-a-city'})
+
+    def test_path_trailing_newlines(self):
+        tests = [
+            '/articles/2003/\n',
+            '/articles/2010/\n',
+            '/en/foo/\n',
+            '/included_urls/extra/\n',
+            '/regex/1/\n',
+            '/users/1/\n',
+        ]
+        for url in tests:
+            with self.assertRaises(Resolver404):
+                resolve(url)
